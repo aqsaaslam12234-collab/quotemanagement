9101850cf83892c3c9c7e4ca4269c3d90a9ba563
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-12-21 09:15:16 +0000

    save

--- src/components/QuoteTable.tsx
+++ src/components/QuoteTable.tsx
@@ -4,7 +4,12 @@ const Quotetable = () => {
     //   const [searchTerm, setSearchTerm] = useState('');
   return (
 <div className="bg-white rounded-lg shadow-md overflow-hidden">
-      <div className="px-6 py-4 bg-gray-50 border-b border-gray-200">
+  
+
+
+
+
+      <div className="px-6 py-4 bg-gray-50 border-b border-teal-200">
         <h3 className="text-xl font-bold text-teal-900 ">All Quotes</h3>
       </div>
          <div className="overflow-x-auto">
@@ -76,36 +81,7 @@ const Quotetable = () => {
 
 
 
-
-
-
-     <div className="bg-white rounded-lg shadow-md overflow-hidden border border-teal-100">
-       <div className="bg-teal-800 px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-4">
-           <h2 className="text-xl font-bold text-white whitespace-nowrap">Qoute System</h2>
-       <div className="relative w-full md:w-64">
-          <input type="text" placeholder="Search Qoute..." className="w-full bg-teal-500 text-white pl-10 pr-4 py-2 rounded-lg focus:outline-none focus:ring-teal-500 placeholder-teal-500 text-sm"
-        // value={searchTerm}
-        // onChange={(e) => setSearchTerm(e.target.value)}
-                />
-         <search className="absolute left-3 top-2.5 w-4 h-4 text-teal-400" />
-           </div>
-         </div>
-        <div className="overflow-x-auto">
-             <table className="w-full">
-               <thead className="bg-teal-50 border-b">
-                   <tr>
-                         <th className="px-6 py-4 text-left text-xs font-semibold text-teal-500 uppercase tracking-wider">Show Qoute</th>
-                         <th className="px-6 py-4 text-left text-xs font-semibold text-teal-500 uppercase tracking-wider hidden lg:table-cell">More Details</th>
-                     </tr>
-                 </thead>
-                 <tbody className="divide-y divide-teal-100">
-
-                 </tbody>
-         </table>
-
-         </div>
-      
-      </div> 
+     
       </div>
 
  )
